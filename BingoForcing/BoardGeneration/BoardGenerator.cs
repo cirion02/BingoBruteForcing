6bf4a32eb52b8e77063286f7225f9004cd74f905
cirion02 <34488326+cirion02@users.noreply.github.com>
2023-03-25 10:24:16 +0100

    Added objective

---
+++ BingoForcing/BoardGeneration/BoardGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace BingoForcing.BoardGeneration;
+
+public class BoardGenerator
+{
+    private Objective[,] _generator;
+
+    public BoardGenerator(Objective[,] generator)
+    {
+        _generator = generator;
+    }
+}
---
+++ BingoForcing/BoardGeneration/Objective.cs
@@ -0,0 +1,13 @@
+﻿namespace BingoForcing.BoardGeneration;
+
+public class Objective
+{
+    public string Name;
+    public string[] Tags;
+
+    public Objective(string name, string[] tags)
+    {
+        Name = name;
+        Tags = tags
+    }
+}
